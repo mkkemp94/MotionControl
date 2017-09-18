f5840c99a5418d6ebbd87c1447598d897f60c59b
Michael Kemp <mkemp@accu-time.com>
2017-09-18 09:25:28 -0400

    Initial commit

---
+++ android/src/com/mygdx/motioncontrols/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.motioncontrols;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.motioncontrols.MotionControls;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MotionControls(), config);
+	}
+}
